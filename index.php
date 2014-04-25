f5a15cd3fd5c1c6aaf46fb8f7be2829606627cdd
hit-user6 <hit_user6@21.cn>
2014-04-25 08:55:55 -0700

    login decoration

--- index.php
+++ index.php
@@ -1,23 +1,79 @@
-<!DOCTYPE html>
-<html>
-<head>
-<SCRIPT>
-function alerme(){
-alert('This web page allows cross-site script, please disable it.');
-}
-</SCRIPT>
-</head>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+	<HEAD>
+		<TITLE> New Document </TITLE>
+		<script type="text/javascript" src="./js/jquery.js"></script>
+		<style type="text/css">
+			body {
+				font: 12px/1.6em "Arial"
+			}
+			ul {
+				list-style: none;
+			}
+			li {
+				margin-top: 10px;
+			}
+		</style>
 
-<body>
+		<script type="text/javascript">
+			$(document).ready(function() { 
+				$("#form").submit(function() {
+					login();				
+					return false;
+				});
+			});
+			function login() {
+				var user = $("#user").val();
+				var pass = $("#pass").val();
+				if (user == "") {
+					$("#confirm").text("Please type in username");
+					$("user").focus();
+					return false;
+				}
+				if (pass == "") {
+					$("#confirm").text("Please type in password");
+					$("#pass").focus();
+					return false;
+				}
+				$.ajax({
+					type : "POST",
+					url : "login.php",
+					data : "id=" + user + "&p=" + pass,
+					beforeSend : function() {
+						$("confirm").text("processing");
+					},
+					success : function(msg) {
+						if (msg == "success") {
+							$("#confirm").text("Wecome back, " + user + ". You are entering the web space.");
+						} else {
+							$("#confirm").text("No such user or password is wrong.");
+						}
+					}
+				});
+			}
+		</script>
 
-<p>First Line Commit</p>
-<div>
-<h1>Cross-Site Script is dangrous! Disable it!!!</h1>
-</div>
-<form>
-  UserName: <input type='text' value='username'></input>
-  PassWord: <input type='password' value='password'></input>
-  <input type='submit' onClick="alertme()" value='go'></input>
-</form>
-</body>
-</html>
+	</HEAD>
+
+	<BODY>
+		<h3>Member Login Page</h3>
+		<div id="login">
+			<form method="post" action="login.php" id="form" name="form">
+				<ul>
+					<li>
+						UserName:
+						<input type="text" name="user" id="user" size="16" maxlength="20">
+					</li>
+					<li>
+						PassWord:
+						<input type="password" name="pass" id="pass" size="16" maxlength="20">
+					</li>
+					<li>
+						<input type="submit" style="border:0" value="go" />
+					</li>
+					<li id="confirm"></li>
+				</ul>
+			</form>
+		</div>
+	</BODY>
+</HTML>
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git init
-git add .
-git commit -m "init"
-git remote add origin https://github.com/eece518/coderelayprj_02.git
-git push origin master
